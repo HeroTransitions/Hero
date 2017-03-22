5fe9a6c247253c6632b3e008989c3b3e61ec1627
Sangjin Lim <limsangjin12@gmail.com>
2017-03-23 02:31:11 +0900

    Indent (#142)

--- Sources/Parser/Regex.swift
+++ Sources/Parser/Regex.swift
@@ -10,23 +10,23 @@ import Foundation
 
 var expressions = [String: NSRegularExpression]()
 public extension String {
-    public func match(regex: String) -> (String, CountableRange<Int>)? {
-        let expression: NSRegularExpression
-        if let exists = expressions[regex] {
-            expression = exists
-        } else {
-            do {
-               expression = try NSRegularExpression(pattern: "^\(regex)", options: [])
-               expressions[regex] = expression
-            } catch {
-              return nil
-            }
-        }
-
-        let range = expression.rangeOfFirstMatch(in: self, options: [], range: NSRange(0 ..< self.utf16.count))
-        if range.location != NSNotFound {
-            return ((self as NSString).substring(with: range), range.location ..< range.location + range.length )
-        }
+  public func match(regex: String) -> (String, CountableRange<Int>)? {
+    let expression: NSRegularExpression
+    if let exists = expressions[regex] {
+      expression = exists
+    } else {
+      do {
+        expression = try NSRegularExpression(pattern: "^\(regex)", options: [])
+        expressions[regex] = expression
+      } catch {
         return nil
+      }
     }
+
+    let range = expression.rangeOfFirstMatch(in: self, options: [], range: NSRange(0 ..< self.utf16.count))
+    if range.location != NSNotFound {
+      return ((self as NSString).substring(with: range), range.location ..< range.location + range.length )
+    }
+    return nil
+  }
 }
