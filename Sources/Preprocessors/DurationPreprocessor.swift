c726f9611aa903037648b40856240ffe31fb86c8
Luke Zhao <me@lkzhao.com>
2017-04-24 13:43:32 -0400

    fix ghosting

--- Sources/Preprocessors/DurationPreprocessor.swift
+++ Sources/Preprocessors/DurationPreprocessor.swift
@@ -18,14 +18,22 @@ class DurationPreprocessor: BasePreprocessor {
     setDurationForInfiniteDuration(views: toViews, duration: maxDuration)
   }
 
+  func optimizedDurationFor(view: UIView) -> TimeInterval {
+    let targetState = context[view]!
+    return view.optimizedDuration(fromPosition: context.container.convert(view.layer.position, from: view.superview),
+                                  toPosition: targetState.position,
+                                  size: targetState.size,
+                                  transform: targetState.transform)
+  }
+
   func applyOptimizedDurationIfNoDuration(views: [UIView]) -> TimeInterval {
     var maxDuration: TimeInterval = 0
     for view in views where context[view] != nil {
       if context[view]?.duration == nil {
-        context[view]!.duration = view.optimizedDuration(targetState: context[view]!)
+        context[view]!.duration = optimizedDurationFor(view: view)
       }
       if context[view]!.duration! == .infinity {
-        maxDuration = max(maxDuration, view.optimizedDuration(targetState: context[view]!))
+        maxDuration = max(maxDuration, optimizedDurationFor(view: view))
       } else {
         maxDuration = max(maxDuration, context[view]!.duration!)
       }
