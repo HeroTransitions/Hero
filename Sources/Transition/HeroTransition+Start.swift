003de286e509a55ec5244c9bfb64273b61ef3d60
Konrad Roj <konradroj@icloud.com>
2017-09-27 17:31:38 +0200

    hotfix flashing navigation controller transitions (iOS11) (#276)
    * hotfix flashing navigation controller transitions (iOS11)
    
    * take snapshot before screen updates
    
    * taking a snapshot before screen updates for transition in navigation controller, iOS11
    
    * make snapshot before screen updates
    
    * hotfix ios11, fixes code style

--- Sources/Transition/HeroTransition+Start.swift
+++ Sources/Transition/HeroTransition+Start.swift
@@ -46,7 +46,7 @@ extension HeroTransition {
     }
 
     // take a snapshot to hide all the flashing that might happen
-    fullScreenSnapshot = transitionContainer?.window?.snapshotView(afterScreenUpdates: true) ?? fromView?.snapshotView(afterScreenUpdates: true)
+    fullScreenSnapshot = transitionContainer?.window?.snapshotView(afterScreenUpdates: false) ?? fromView?.snapshotView(afterScreenUpdates: false)
     if let fullScreenSnapshot = fullScreenSnapshot {
       (transitionContainer?.window ?? transitionContainer)?.addSubview(fullScreenSnapshot)
     }
