5d03c704388869150da77f9aff5d9159e01e4b9d
Josh Channings <josh.channings@music-group.com>
2017-09-28 16:09:37 +0100

    Update Package.swift for Swift 4.0

--- Package.swift
+++ Package.swift
@@ -1,5 +1,15 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-    name: "Hero"
+    name: "Hero",
+    products: [
+        .library(name: "Hero",  targets: ["Hero"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "Hero", path: "Sources")
+    ]
 )
