b433d092d59557d2fa75797b58abbeba794bd0d2
calm <calm@nb.com>
2018-10-20 18:19:58 +0800

    MainViewController.swift, MatchInCollectionExample.swift, AppStoreCardExample.swift, update adaptation CollectionKit >2.0

--- Examples/MainViewController.swift
+++ Examples/MainViewController.swift
@@ -2,60 +2,71 @@ import UIKit
 import CollectionKit
 
 class MainViewController: UIViewController {
+  
+  typealias SourceData = (UIViewController.Type, String)
+  
   let collectionView = CollectionView()
-  let examples: [(UIViewController.Type, String)] = [
-    (BuiltInTransitionExampleViewController1.self, "Built In Animations"),
-    (MatchExampleViewController1.self, "Match Animation"),
-    (MatchInCollectionExampleViewController1.self, "Match Cell in Collection"),
-    (AppStoreViewController1.self, "App Store Transition"),
-  ]
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .white
-
-    let examplesProvider = CollectionProvider(
-      data: examples,
-      viewUpdater: { (label: UILabel, data, index) in
-        label.text = "\(index + 1). \(data.1)"
-        label.textAlignment = .center
-        label.layer.borderColor = UIColor.gray.cgColor
-        label.layer.borderWidth = 0.5
-        label.layer.cornerRadius = 8
-      },
-      layout: FlowLayout(lineSpacing: 10),
-      sizeProvider: { (index, data, collectionSize) in
-        return CGSize(width: collectionSize.width, height: 64)
-      },
-      tapHandler: { [unowned self] (label, index, dataProvider) in
-        let vc = dataProvider.data(at: index).0.init()
-        self.present(vc, animated: true, completion: nil)
-      }
-    )
-
+    
+    view.addSubview(collectionView)
+    
+    setupcollection()
+  }
+  
+  func setupcollection() {
+    let dataSource = ArrayDataSource<SourceData>(data: [
+      (BuiltInTransitionExampleViewController1.self, "Built In Animations"),
+      (MatchExampleViewController1.self, "Match Animation"),
+      (MatchInCollectionExampleViewController1.self, "Match Cell in Collection"),
+      (AppStoreViewController1.self, "App Store Transition"),
+      ])
+    
+    let viewSource = ClosureViewSource { (label: UILabel, data: SourceData, index) in
+      label.text = "\(index + 1). \(data.1)"
+      label.textAlignment = .center
+      label.layer.borderColor = UIColor.gray.cgColor
+      label.layer.borderWidth = 0.5
+      label.layer.cornerRadius = 8
+    }
+    
+    let sizeSource = { (i: Int, data: SourceData, size: CGSize) -> CGSize in
+      return CGSize(width: size.width, height: 64)
+    }
+    
+    let examplesProvider = BasicProvider<SourceData, UILabel>(
+      dataSource: dataSource,
+      viewSource: viewSource,
+      sizeSource: sizeSource,
+      layout: FlowLayout(lineSpacing: 10))
+    { (context) in
+      let vc = context.data.0.init()
+      self.present(vc, animated: true, completion: nil)
+    }
     // TODO: Migrate the example to CollectionKit 2.2.0
-
+    
     let imageView = UIImageView(image: #imageLiteral(resourceName: "HeroLogo"))
     imageView.contentMode = .scaleAspectFit
-    //let imageProvider = ViewCollectionProvider(imageView, sizeStrategy: (.fill, .fit))
-
+    
+    let imageProvider = SimpleViewProvider(views: [imageView], sizeStrategy: (.fill, .fit))
+    
     let legacyButton = UIButton(type: .system)
     legacyButton.setTitle("Legacy Examples", for: .normal)
     legacyButton.addTarget(self, action: #selector(showLegacy), for: .touchUpInside)
-    //let legacyExamplesProvider = ViewCollectionProvider(legacyButton, sizeStrategy: (.fill, .fit))
-
-//    collectionView.provider = CollectionComposer(
-//      layout: FlowLayout(lineSpacing: 10).inset(by: UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)),
-//      sections: [imageProvider, examplesProvider, legacyExamplesProvider]
-//    )
-
-    view.addSubview(collectionView)
+    let legacyExamplesProvider = SimpleViewProvider(views: [legacyButton], sizeStrategy: (.fill, .fit))
+    
+    collectionView.provider = ComposedProvider(
+      layout: FlowLayout(lineSpacing: 10).inset(by: UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)),
+      sections: [imageProvider, examplesProvider, legacyExamplesProvider]
+    )
   }
-
+  
   @objc func showLegacy() {
     hero.replaceViewController(with: viewController(forStoryboardName: "Main"))
   }
-
+  
   override func viewDidLayoutSubviews() {
     super.viewDidLayoutSubviews()
     collectionView.frame = view.bounds
