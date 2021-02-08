fd49ad46fb8d1e82c07ca3220db680e3d8329ca1
Joseph Mattiello <mail@joemattiello.com>
2021-02-07 19:39:14 -0500

    Run Swift 5 Migration Tool

--- Sources/HeroContext.swift
+++ Sources/HeroContext.swift
@@ -272,7 +272,7 @@ extension HeroContext {
      let pairedView = pairedView(for: view),
      let pairedSnapshot = snapshotViews[pairedView],
      let siblingViews = pairedView.superview?.subviews,
-      let index = siblingViews.firstIndex(of: pairedView) {
+     let index = siblingViews.firstIndex(of: pairedView) {
       let nextSiblings = siblingViews[index+1..<siblingViews.count]
       containerView.addSubview(pairedSnapshot)
       containerView.addSubview(snapshot)
