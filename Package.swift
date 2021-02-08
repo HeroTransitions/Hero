46d43e41c3bf5cb75cd0e0b1ebf772e388cf3aa4
Joseph Mattiello <mail@joemattiello.com>
2021-02-08 00:07:13 -0500

    swift pm add CLI tools

--- Package.swift
+++ Package.swift
@@ -3,17 +3,26 @@
 
 import PackageDescription
 
+let ciDependencies: [Package.Dependency] = [
+    .package(url: "https://github.com/danger/swift.git", from: "1.0.0"),
+    .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.35.8"),
+    .package(url: "https://github.com/Realm/SwiftLint", from: "0.28.1"),
+    .package(url: "https://github.com/orta/Komondor", from: "1.0.0")]
+
+let dependencies = ciDependencies
+
 let package = Package(
     name: "Hero",
     platforms: [
-        .tvOS(.v9),
-        .iOS(.v9)
+        .tvOS(.v10),
+        .iOS(.v10)
     ],
     products: [
         .library(name: "Hero",
                  type: .dynamic,
                  targets: ["Hero"]),
     ],
+    dependencies: dependencies,
     targets: [
         .target(name: "Hero", path: "Sources"),
         .testTarget(name: "HeroTests",
@@ -22,3 +31,20 @@ let package = Package(
     ],
     swiftLanguageVersions: [.v5]
 )
+
+// The settings for the git hooks for our repo
+#if canImport(PackageConfig)
+    import PackageConfig
+
+    let config = PackageConfig([
+        "komondor": [
+            // When someone has run `git commit`, first run
+            // SwiftFormat and the auto-correcter for SwiftLint
+            "pre-commit": [
+                "swift run swiftformat .",
+                "swift run swiftlint autocorrect",
+                "git add .",
+            ],
+        ]
+    ])
+#endif
